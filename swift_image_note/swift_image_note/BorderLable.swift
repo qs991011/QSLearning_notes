c8d41758aba22cf25a1eb0dd0d982dc41850daf4
胜的钱 <qianshengsheng@96900.com.cn>
2018-08-06 14:25:31 +0800

    练习

--- swift_image_note/swift_image_note/BorderLable.swift
+++ swift_image_note/swift_image_note/BorderLable.swift
@@ -25,3 +25,69 @@ class BorderLable: UILabel {
         self.height += 5
     }
 }
+
+enum  QSGestureRecognizerState : Int{
+    case  Began = 0 , Moved , Ended , Cancelled
+}
+
+class GCIControl : UIView {
+    var longPressDetected = false
+    var imageView : UIImageView!
+    
+    var image : UIImage? {
+        set {
+            self.layer.contents = image!.cgImage
+        }
+        get {
+            var _image : UIImage?
+            
+            let content = self.layer.contents as! CGImage
+            
+            if content != self.image?.cgImage {
+                _image = UIImage(cgImage: content , scale: self.layer.contentsScale, orientation: .up)
+                
+            } else {
+                _image = nil
+            }
+            return _image
+            
+        }
+    }
+    
+    var touchBlock : ((_ view:GCIControl,_ state : QSGestureRecognizerState, _ touches : Set<UITouch>, _ event:UIEvent?)->Void )?
+    
+    var longPressBlock : ((_ view : GCIControl,_ point : CGPoint) -> CGPoint)?
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        imageView = UIImageView(frame: self.bounds)
+        imageView.isUserInteractionEnabled = true
+        self.addSubview(imageView)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(touches)
+        longPressDetected = false
+        touchBlock?(self,.Began,touches,event)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if longPressDetected {return}
+        touchBlock?(self,.Moved,touches,event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if longPressDetected {return}
+        touchBlock?(self,.Ended,touches,event)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if longPressDetected {return}
+        touchBlock?(self,.Cancelled,touches,event)
+    }
+    
+}
--- swift_image_note/swift_image_note/ViewController.swift
+++ swift_image_note/swift_image_note/ViewController.swift
@@ -8,23 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let a = Array<Any>()
+//        let fv = GCIControl(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//        fv.backgroundColor = UIColor.red
+//        self.view.addSubview(fv)
         
+        
+        let table = UITableView(frame: self.view.bounds, style: .plain)
+        table.separatorStyle = .none
+        table.delegate  = self
+        table.dataSource = self
+        table.rowHeight = 60
+        self.view.addSubview(table)
         
     
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        let v = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 60))
+        
+        let fv = GCIControl(frame: CGRect(x: 0, y: 0, width: 100, height: 60))
+        v.addSubview(fv)
+        fv.backgroundColor = UIColor.red
+    
+        cell.contentView.addSubview(fv)
+        return cell
+        
+    }
+    
 
 
 }
